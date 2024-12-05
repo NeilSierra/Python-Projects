0d90cfec89c9a9a53e233c06ee9b677d219539dc
Neil Sierra <sierra.neilalfredngis@gmail.com>
2024-12-05 14:39:20 +0800

    new

--- airlines.py
+++ airlines.py
@@ -65,7 +65,7 @@ sysDict = {
     # Destination airports - excluded the origin airport
     "airport1": {
       "duration": 8,
-      "flight1": [12,  1, 2024,  8, model1, 400, 372],
+      1: [12,  1, 2024,  8, model1, 400, 372],
       2: [12,  6, 2024, 10, model2, 410, 317],
       3: [12, 11, 2024, 12, model3, 420, 252],
       4: [12, 16, 2024, 14, model4, 430, 234],
@@ -74,16 +74,16 @@ sysDict = {
     },
     "airport3": {
       "duration": 12,
-      "flight1": [12,  2, 2024, 20, model3,   12, 720, 287],
-      2: [12,  7, 2024, 22, model4,   12, 730, 203],
-      3: [12, 12, 2024, 24, model1,   12, 700, 368],
-      4: [12, 17, 2024,  2, model2,   12, 710, 305],
-      5: [12, 22, 2024,  4, model3,   12, 720, 255],
-      6: [12, 27, 2024,  6, model4,   12, 730, 225]
+      1: [12,  2, 2024, 20, model3, 720, 287],
+      2: [12,  7, 2024, 22, model4, 730, 203],
+      3: [12, 12, 2024, 24, model1, 700, 368],
+      4: [12, 17, 2024,  2, model2, 710, 305],
+      5: [12, 22, 2024,  4, model3, 720, 255],
+      6: [12, 27, 2024,  6, model4, 730, 225]
     },
     "airport4": {
       "duration": 13,
-      "flight1": [12,  3, 2024,  8, model1, 700, 395],
+      1: [12,  3, 2024,  8, model1, 700, 395],
       2: [12,  8, 2024, 10, model2, 710, 322],
       3: [12, 13, 2024, 12, model3, 720, 296],
       4: [12, 18, 2024, 14, model4, 730, 214],
@@ -92,21 +92,21 @@ sysDict = {
     },
     "airport5": {
       "duration": 2.5,
-      "flight1": [12,  4, 2024, 20, model3,  2.5, 170, 267],
-      2: [12,  9, 2024, 22, model4,  2.5, 180, 207],
-      3: [12, 14, 2024, 24, model1,  2.5, 150, 349],
-      4: [12, 19, 2024,  2, model2,  2.5, 160, 328],
-      5: [12, 24, 2024,  4, model3,  2.5, 170, 253],
-      6: []
+      1: [12,  4, 2024, 20, model3, 170, 267],
+      2: [12,  9, 2024, 22, model4, 180, 207],
+      3: [12, 14, 2024, 24, model1, 150, 349],
+      4: [12, 19, 2024,  2, model2, 160, 328],
+      5: [12, 24, 2024,  4, model3, 170, 253],
+      6: [12, 29, 2024, 10, model4, 830, 224]
     },
     "airport6": {
       "duration": 4.5,
-      "flight1": [12,  5, 2024,  8, model1, 300, 362],
+      1: [12,  5, 2024,  8, model1, 300, 362],
       2: [12, 10, 2024, 10, model2, 310, 343],
       3: [12, 15, 2024, 12, model3, 320, 270],
       4: [12, 20, 2024, 14, model4, 330, 222],
       5: [12, 25, 2024, 16, model1, 300, 366],
-      6: []
+      6: [12, 30, 2024, 22, model2, 810, 312]
     },
   },
   "airport3": {
@@ -115,7 +115,7 @@ sysDict = {
     # Destination airports - excluded the origin airport
     "airport1": {
       "duration": 7,
-      "flight1": [12,  1, 2024, 12, model1, 500, 396],
+      1: [12,  1, 2024, 12, model1, 500, 396],
       2: [12,  6, 2024, 14, model2, 510, 323],
       3: [12, 11, 2024, 16, model3, 520, 273],
       4: [12, 16, 2024, 18, model4, 530, 240],
@@ -124,7 +124,7 @@ sysDict = {
     },
     "airport2": {
       "duration": 11.5,
-      "flight1": [12,  2, 2024, 24, model3, 720, 246],
+      1: [12,  2, 2024, 24, model3, 720, 246],
       2: [12,  7, 2024,  2, model4, 730, 200],
       3: [12, 12, 2024,  4, model1, 700, 352],
       4: [12, 17, 2024,  6, model2, 710, 301],
@@ -133,7 +133,7 @@ sysDict = {
     },
     "airport4": {
       "duration": 11,
-      "flight1": [12,  3, 2024, 12, model1, 700, 385],
+      1: [12,  3, 2024, 12, model1, 700, 385],
       2: [12,  8, 2024, 14, model2, 710, 339],
       3: [12, 13, 2024, 16, model3, 720, 254],
       4: [12, 18, 2024, 18, model4, 730, 233],
@@ -142,7 +142,7 @@ sysDict = {
     },
     "airport5": {
       "duration": 14,
-      "flight1": [12,  4, 2024, 24, model3, 820, 290],
+      1: [12,  4, 2024, 24, model3, 820, 290],
       2: [12,  9, 2024,  2, model4, 830, 208],
       3: [12, 14, 2024,  4, model1, 800, 349],
       4: [12, 19, 2024,  6, model2, 810, 333],
@@ -151,7 +151,7 @@ sysDict = {
     },
     "airport6": {
       "duration": 11,
-      "flight1": [12,  5, 2024, 12, model1, 800, 375],
+      1: [12,  5, 2024, 12, model1, 800, 375],
       2: [12, 10, 2024, 14, model2, 810, 300],
       3: [12, 15, 2024, 16, model3, 820, 283],
       4: [12, 20, 2024, 18, model4, 830, 220],
@@ -165,7 +165,7 @@ sysDict = {
     # Destination airports - excluded the origin airport
     "airport1": {
       "duration": 15,
-      "flight1": [12,  1, 2024, 16, model1, 900, 380],
+      1: [12,  1, 2024, 16, model1, 900, 380],
       2: [12,  6, 2024, 18, model2, 910, 310],
       3: [12, 11, 2024, 20, model3, 920, 270],
       4: [12, 16, 2024, 22, model4, 930, 209],
@@ -174,7 +174,7 @@ sysDict = {
     },
     "airport2": {
       "duration": 13,
-      "flight1": [12,  2, 2024,  4, model3, 720, 251],
+      1: [12,  2, 2024,  4, model3, 720, 251],
       2: [12,  7, 2024,  6, model4, 730, 204],
       3: [12, 12, 2024,  8, model1, 700, 358],
       4: [12, 17, 2024, 10, model2, 710, 318],
@@ -310,161 +310,228 @@ sysDict = {
     }
   },
   "class": {
-    1: ["Economy Class", "ECLASS", 1],
+    1: ["Economy Class",  "ECLASS",   1],
     2: ["Business Class", "BCLASS", 1.5],
-    3: ["First Class", "FCLASS", 2.5]
+    3: ["First Class",    "FCLASS", 2.5]
   }
 }
 
-def getOrigin():
-  global airportList, origin
-  airportList = ["airport1", "airport2", "airport3", "airport4", "airport5", "airport6"]
-  # Prompt message
-  print("\n✈️   Hello and Welcome to Python Ticketing Airlines! 🌍" +
-        "\n\nHere we serve our travelers with the outmost..." +
-        "\n⚡ Efficiency, 🛡️  Safety and 🔐 Security!" +
-        "\n\n" + "=" * 100 +
-        "\n\n📍 Choose an ORIGIN airport:")
+# Function for getting the chosen route
+def getRoute():
+  global origin, destination, route
+  # Welcome and prompt message
+  print("\n\n\n✈️  Hello and Welcome to Python Ticketing Airlines!🌍" +
+        f"\n{"_" * 70}" +
+        "\n\n📍 Here are the available airports:")
+  # Prints all the choices
   for n in range(len(airportList)):
     print(f"   {n + 1}. {sysDict[airportList[n]]["name"]}")
+  # Prompt for restarting the system
+  print("\n   Type 'X' to restart the selection.")
+  # Loop for user input
   while True:
+    # Error handling for user input
     try:
-      userInput = input(f"\n>> 📋 Enter choice (1-{len(airportList)}): ")
-      if 1 <= int(userInput) <= len(airportList):
-        origin = airportList[int(userInput) - 1]
-        airportList.remove(origin)
+      # Request for user input
+      inputORG = input(f"\n>> 📋 Choose an ORIGIN airport (1-{len(airportList)}): ")
+      # Check if input is in range of choices
+      if 1 <= int(inputORG) <= len(airportList):
+        # Store the input in a variable
+        origin = airportList[int(inputORG) - 1]
+        # Feedback message
         print(f">> 🛫 Origin Airport: {sysDict[origin]["name"]}")
         break
-      print(f">> ❌ Invalid input: '{userInput}'")
+      else:
+        # Error when the input is not in range of choices
+        print(">> ❌ Invalid Input! Please enter a number in range of choices!")
     except Exception as e:
-      print(f">> ❌ Error: {e}")
-
-def getDestination():
-  global destination
-  print("\n" + "=" * 100 +
-        "\n\n📍 Choose an ORIGIN airport:")
-  for n in range(len(airportList)):
-    print(f"   {n + 1}. {sysDict[airportList[n]]["name"]}")
-  print("\n   Type 'X' to restart the selection.")
+      # Restarts the program
+      if inputORG.lower() == "x": return True
+      # Error when the input is not a number
+      print(f">> ❌ Invalid Input: {e}")
+  # Loop for user input
   while True:
+    # Error handling for user input
     try:
-      userInput = input(f"\n>> 📋 Enter choice (1-{len(airportList)}): ")
-      if userInput.lower() == 'x':
-        print(">> 🔄 Restarting selection...")
-        return True
-      elif 1 <= int(userInput) <= len(airportList):
-        destination = airportList[int(userInput) - 1]
+      # Request for user input
+      inputDST = input(f"\n>> 📋 Choose a DESTINATION airport (1-{len(airportList)}): ")
+      # Check if input is in range of choices
+      if inputDST == inputORG:
+        print(">> ❌ Invalid Input! Cannot select origin as destination!")
+      elif 1 <= int(inputDST) <= len(airportList):
+        # Store the input in a variable
+        destination = airportList[int(inputDST) - 1]
+        # Feedback message
         print(f">> 🛬 Destination Airport: {sysDict[destination]["name"]}")
-        return False
-      print(f">> ❌ Invalid input: '{userInput}'")
+        break
+      else:
+        # Error when the input is not in range of choices
+        print(">> ❌ Invalid Input! Please enter a number in range of choices!")
     except Exception as e:
-      print(f">> ❌ Error: {e}")     
-
-def time(x):
-  if x == 24.0:
-    return "12:00 AM"
-  elif x == 12:
-    return "12:00 PM"
-  hr = int(x // 1)
-  mn = int((x - hr) * 60)
-  if x < 12:
-    return f"{hr:02d}:{mn:02d} AM"
-  else:
-    return f"{hr:02d}:{mn:02d} PM"
-
-def seats(x, y):
-  if x == y:
-    return "❌ FULL"
-  return f"{x}/{y}"
-
-def getFlightSchedule():
-  global route, flightSched
-  print("\n" + "=" * 100 +
-        f"\n\n               {"Airport":<44}   Location" +
-        f"\nOrigin:        {sysDict[origin]["name"]:<44}   {sysDict[origin]["location"]}" +
-        f"\nDestination:   {sysDict[destination]["name"]:<44}   {sysDict[destination]["location"]}" +
-        f"\n\n📍 Choose a FLIGHT SCHEDULE:")
+      # Restarts the program
+      if inputDST.lower() == "x": return True
+      # Error when the input is not a number
+      print(f">> ❌ Invalid Input: {e}")
+  # Set the route variable for future uses
   route = sysDict[origin][destination]
-  for n in range(6):
+  duration = "⌛ Flight Duration: ~ " + str(route["duration"]) + " Hours"
+  print(f"\n   +------------------------------------------------------+" +
+        f"\n   | {"🗺️  Flight Route:":^53} |" +
+        f"\n   +------+-----------------------------------------------+" +
+        f"\n   | {"From":^4} | {sysDict[origin]["name"]:<45} |" +
+        f"\n   |      | {sysDict[origin]["location"]:<45} |" +
+        f"\n   +------+-----------------------------------------------+" +
+        f"\n   | {"To":^4} | {sysDict[destination]["name"]:<45} |" +
+        f"\n   |      | {sysDict[destination]["location"]:<45} |" +
+        f"\n   +------+-----------------------------------------------+" +
+        f"\n   | {duration:^51} |" +
+        f"\n   +------------------------------------------------------+" +
+        f"\n{"_" * 70}")
+
+# Function to compute for the date of the flight
+calcDate = lambda x : f"{x[0]:02d}/{x[1]:02d}/{x[2]}"
+
+# Function to compute for the take-off time of the flight schedule
+def calcTime(x):
+  # Converts number into time
+  if x[3] == 24:
+    return f"12:00 AM"
+  elif x[3] == 12:
+    return f"12:00 PM"
+  hr = x[3] // 1
+  mn = (x[3] - hr) * 60
+  hr = f"{int(hr):02d}"
+  mn = f"{int(mn):02d}"
+  if x[3] > 12:
+    return f"{hr}:{mn} PM"
+  elif x[3] < 12:
+    return f"{hr}:{mn} AM"
+
+# Function to compute for the flight schedule seats
+def calcSeats(x):
+  if x[6] == x[4][0]: return "❌ FULL"
+  else: return f"{x[6]}/{x[4][0]}"
+  
+# Fucntion to get the chosen flight schedule
+def getSchedule():
+  global schedule
+  # Prompt message
+  print(f"\n   🛫 Flight Schedules:\n")
+  # Prints all choices
+  for n in range(len(route) - 1):
     n += 1
     sched = route[n]
-    print(f"   {n}. {sched[0]:02d}/{sched[1]:02d}/{sched[2]} | {time(sched[3])} | {sched[4][1]:<21} | ${sched[5]:.2f} | {seats(sched[6], sched[4][0])}")
-  print("\n   Type 'X' to restart the selection.")
+    cost = f"Cost: ${sched[5]:.2f}"
+    print(f"    {n}.   {calcDate(sched)} - {calcTime(sched)}    {sched[4][1]:^24}" +
+          f"\n         {cost:^21}    {calcSeats(sched):^24}\n")
+  # Prompt for restarting the system
+  print("   Type 'X' to restart the selection.")
+  # Loop for user input
   while True:
+    # Error handling for user input
     try:
-      userInput = input(f"\n>> 📋 Enter choice (1-{len(airportList)}): ")
-      if userInput.lower() == 'x':
-        print(">> 🔄 Restarting selection...")
-        return True
-      elif 1 <= int(userInput) <= len(airportList):
-        flightSched = route[int(userInput)]
-        print(f">> 🛬 Flight Schedule: {flightSched[0]:02d}/{flightSched[1]:02d}/{flightSched[2]} - {time(flightSched[3])}")
-        return False
-      print(f">> ❌ Invalid input: '{userInput}'")
+      # Request for user input
+      inputFlightSchedule = input(f"\n>> 📋 Choose a FLIGHT SCHEDULE (1-{len(route) - 1}): ")
+      # Check if input is in range of choices
+      if 1 <= int(inputFlightSchedule) <= len(route) - 1:
+        # Sets the schedule from the input
+        schedule = route[int(inputFlightSchedule)]
+        # Checks if the chosen schedule is full or not
+        if schedule[6] == schedule[4][0]:
+          # Error when the schedule is full
+          print(">> ❌ Invalid Input! Chosen flight schedule is full!")
+          continue
+        else:
+          # Feedback message of the chosen flight schedule
+          print(f">> 🛫 Flight Schedule: {calcDate(schedule)} - {calcTime(schedule)}" +
+                f"\n{"_" * 70}")
+          break
     except Exception as e:
-      print(f">> ❌ Error: {e}")
+      # Restarts the program
+      if inputFlightSchedule.lower() == "x": return True
+      # Error when the input is not a number
+      print(f">> ❌ Invalid Input: {e}")
 
-def getTicketAmount():
-  global ticketAmount
-  print("\n🛫 Flight Schedule:" +
-        f"\n   > Date:      {flightSched[0]:02d}/{flightSched[1]:02d}/{flightSched[2]}" +
-        f"\n   > Take-off:  {time(flightSched[3])}" +
-        f"\n   > Duration:  {route["duration"]} Hours" +
-        f"\n   > Aircraft:  {flightSched[4][1]}" +
-        f"\n   > Price:     ${flightSched[5]:.2f}" +
-        f"\n   > Seats:     {seats(flightSched[6], flightSched[4][0])}" +
-        "\n\n   Type 'X' to restart the selection.")
-  while True:
-    try:
-      seatsLeft = flightSched[4][0] - flightSched[6]
-      userInput = input(f"\n>> 🛒 How many tickets would you like to buy? ({seatsLeft} left): ")
-      if userInput.lower() == 'x':
-        print(">> 🔄 Restarting selection...")
-        return True
-      elif 1 <= int(userInput) <= seatsLeft:
-        ticketAmount = int(userInput)
-        print(f">> 🎫 Ticket amount: {ticketAmount} Ticket/s")
-        return False
-      print(f">> ❌ Invalid input: '{userInput}'")
-    except Exception as e:
-      print(f">> ❌ Error: {e}")
-
-def getTicketClass():
-  global ticketClass, totalCost
-  print(f"\n📍 Choose a TICKET CLASS:")
-  for n in range(3):
+# Function the get the ticket class and the amount
+def getTicket():
+  global ticketClass, ticketAmount
+  # Prompt message
+  print("\n   📍 Choose a TICKET CLASS:\n")
+  # Prints all choices
+  for n in range(len(sysDict["class"])):
     n += 1
-    print(f"   {n}. {sysDict['class'][n][0]:<20} Total: ${flightSched[5] * ticketAmount * sysDict['class'][n][2]:.2f}")
-  print("\n   Type 'X' to restart the selection.")
+    ticketClass = sysDict["class"][n]
+    print(f"   {n}. {ticketClass[0]} ({ticketClass[2]}x)" +
+          f"\n      Cost per Ticket: ${schedule[5] * ticketClass[2]:.2f}\n")
+  # Prompt for restarting the system
+  print("   Type 'X' to restart the selection.")
+  # Error handling for user input
   while True:
     try:
-      userInput = input(f"\n>> 📋 Enter choice (1-{len(sysDict['class'])}): ")
-      if userInput.lower() == 'x':
-        print(">> 🔄 Restarting selection...")
-        return True
-      elif 1 <= int(userInput) <= len(sysDict['class']):
-        ticketClass = sysDict['class'][int(userInput)]
-        totalCost = flightSched[5] * ticketAmount * ticketClass[2]
-        print(f">> 🎫 Ticket Class: {ticketClass[0]}" +
-              f"\n>> 💵 Total Cost:   ${totalCost:.2f}")
-        return False
-      print(f">> ❌ Invalid input: '{userInput}'")
+      # Request for user input
+      inputTicketClass = input(f"\n>> 📋 Choose a TICKET CLASS (1-{len(ticketClass)}): ")
+      # Check if input is in range of choices
+      if 1 <= int(inputTicketClass) <= len(ticketClass):
+        # Sets the ticket class from the input
+        ticketClass = sysDict["class"][int(inputTicketClass)]
+        # Feedback message of the chosen ticket class
+        print(f">> 🎫 Ticket Class: {ticketClass[0]}")
+        break
     except Exception as e:
-      print(f">> ❌ Error: {e}")
+      # Restarts the program
+      if inputTicketClass.lower() == "x": return True
+      # Error when the input is not a number
+      print(f">> ❌ Invalid Input: {e}")
+  seatsLeft = schedule[4][0] - schedule[6]
+  # Error handling for user input
+  while True:
+    try:
+      # Request for user input
+      inputTicketAmount = input(f"\n>> 📋 Enter TICKET AMOUNT ({seatsLeft} left): ")
+      # Check if input is in range of choices
+      if 1 <= int(inputTicketAmount) <= seatsLeft:
+        # Sets the ticket amount from the input
+        ticketAmount = int(inputTicketAmount)
+        # Feedback message of the chosen ticket class
+        print(f">> 🎫 Ticket Amount: {ticketAmount}" +
+              f"\n{"_" * 70}")
+        break
+    except Exception as e:
+      # Restarts the program
+      if inputTicketAmount.lower() == "x": return True
+      # Error when the input is not a number
+      print(f">> ❌ Invalid Input: {e}")
 
-restart_message = "\n" + "=" * 100 + f"\n{"Restarting System...":^100}" + "\n" + "=" * 100
+# Function to get payment from the user
+def getPayment():
+  totalCost = ticketClass[2] * ticketAmount * schedule[5]
+  print("\n🎫 Ticket Details:" +
+        f"\n   - Ticket Cost:     ${schedule[5]:.2f}"
+        f"\n   - Ticket Class:    {ticketClass[0]}" +
+        f"\n   - Ticket Amount:   {ticketAmount}" +
+        f"\n   - Total Cost:      ${totalCost:.2f}" +
+        f"\n{"_" * 70}")
+   # Error handling for user input
+  while True:
+    try:
+      # Request for user input
+      inputPaymentAmount = input(f"\n>> 💵 Enter PAYMENT AMOUNT: $")
+      # Check if input is greater than total cost
+      if float(inputPaymentAmount) >= totalCost:
+        # Sets the payment amount from the input
+        paymentAmount = float(inputPaymentAmount)
+        # Feedback message of the chosen ticket class
+        print(f">> 🎫 Change Amount: {paymentAmount - totalCost}" +
+              f"\n{"_" * 70}")
+        break
+    except Exception as e:
+      # Restarts the program
+      if inputPaymentAmount.lower() == "x": return True
+      # Error when the input is not a number
+      print(f">> ❌ Invalid Input: {e}")
 
 while True:
-  getOrigin()
-  if getDestination():
-    print(restart_message)
-    continue
-  if getFlightSchedule():
-    print(restart_message)
-    continue
-  if getTicketAmount():
-    print(restart_message)
-    continue
-  if getTicketClass():
-    print(restart_message)
-    continue
+  if getRoute(): continue
+  if getSchedule(): continue
+  if getTicket(): continue
+  if getPayment(): continue
