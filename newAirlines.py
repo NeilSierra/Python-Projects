566562dab6053fa3a356ba9835d6ea9519b0ed4b
Neil Sierra <sierra.neilalfredngis@gmail.com>
2024-12-12 18:30:07 +0800

    a

--- newAirlines.py
+++ newAirlines.py
@@ -12,63 +12,189 @@ airports = {
     # Routes Available
     2: {
       #   Date        Take-off    $$   PAX  Aircraft
-      1: ["12/01/24", "04:00 AM", 400, 100, aircrafts[1]],
-      2: ["12/08/24", "08:00 AM", 400, 100, aircrafts[2]],
-      3: ["12/15/24", "12:00 PM", 400, 100, aircrafts[3]]
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
     },
-    3: {},
-    4: {},
-    5: {},
-    6: {}
+    3: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    4: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    5: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    6: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    }
   },
   2: {
     "name": "CHN - Guangzhou Baiyun International Airport",
     "location": "Guangdong Province, China",
     # Routes Available
-    1: {},
-    3: {},
-    4: {},
-    5: {},
-    6: {}
+    1: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    3: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    4: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    5: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    6: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    }
   },
   3: {
     "name": "GBR - London Heathrow Airport",
     "location": "London, United Kingdom",
     # Routes Available
-    1: {},
-    2: {},
-    4: {},
-    5: {},
-    6: {}
+    1: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    2: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    4: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    5: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    6: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    }
   },
   4: {
     "name": "USA - Los Angeles International Airport",
     "location": "California, USA",
     # Routes Available
-    1: {},
-    2: {},
-    3: {},
-    5: {},
-    6: {}
+    1: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    3: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    3: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    5: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    6: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    }
   },
   5: {
     "name": "PHL - Ninoy Aquino International Airport",
     "location": "Manila, Philippines",
     # Routes Available
-    1: {},
-    2: {},
-    3: {},
-    4: {},
-    6: {}
+    1: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    3: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    3: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    4: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    6: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    }
   },
   6: {
     "name": "JPN - Tokyo Haneda Airport",
     "location": "Tokyo, Japan",
     # Routes Available
-    1: {},
-    2: {},
-    3: {},
-    4: {},
-    5: {}
+    1: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    3: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    3: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    4: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    },
+    5: {
+      #   Date        Take-off    $$   PAX  Aircraft
+      1: ["12/01/24", "04:00 AM", 500, 100, aircrafts[1]]
+    }
   }
 }
+
+def getRoute():
+  global origin, destination
+  print("\nHello and Welcome to Python Airlines!")
+  print("\nAirports List:")
+  for n in range(len(airports)):
+    n += 1
+    print(f"{n}. {airports[n]["name"]}")
+  while True:
+    org = input("\nChoose an Origin: ")
+    if org.isdigit() and 1 <= int(org) <= len(airports):
+      origin = int(org)
+      print(f"Chosen {airports[origin]["name"]} as the origin airport.")
+      break
+    elif org == "x":
+      print("Restarting...")
+      return True
+    else:
+      print("Invalid input! Please try again.")
+  while True:
+    dtn = input("\nChoose an Destination: ")
+    if dtn == org:
+      print("Invalid input! Cannot choose origin as destination.")
+    elif dtn.isdigit() and 1 <= int(dtn) <= len(airports):
+      destination = int(dtn)
+      print(f"Chosen {airports[destination]["name"]} as the origin airport.")
+      break
+    elif dtn == "x":
+      print("Restarting...")
+      return True
+    else:
+      print("Invalid input! Please try again.")
+
+def getFlight():
+  print("\nFlight Schedule:")
+  for n in range(len(airports[origin][destination])):
+    n += 1
+    print(f"{n} {airports[origin][destination][n]}")
+
+while True:
+  if getRoute(): continue
