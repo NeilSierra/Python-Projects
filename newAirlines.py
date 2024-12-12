8c4681640e666a7cedaa0470a90f827b7a8b0180
Neil Sierra <sierra.neilalfredngis@gmail.com>
2024-12-12 09:49:50 +0800

    a

---
+++ newAirlines.py
@@ -0,0 +1,80 @@
+airports = {
+  1: {
+    "name": "UAE - Dubai International Airport",
+    "location": "Dubai, United Arab Emirates",
+    # Routes Available
+    2: {
+      "duration": 7.5,
+      1: ["12/01/2024", "04:00 AM", 100, 100, 122, "Airbus A320 Family"],
+      2: ["12/01/2024", "04:00 AM", 100, 100, 148, "Boeing 787 Dreamliner"],
+      3: ["12/01/2024", "04:00 AM", 100, 100, 175, "Airbus A350 XWB"],
+      4: ["12/01/2024", "04:00 AM", 100, 100, 198, "Boeing 777"]
+    },
+    3: {
+      "duration": 8,
+      1: ["12/02/2024", 16, model3, 520, 295]
+    },
+    4: {
+      "duration": 15,
+      1: ["12/02/2024", 16, model3, 520, 295]
+    },
+    5: {
+      "duration": 8.5,
+      1: ["12/04/2024", 16, model3, 420, 278]
+    },
+    6: {
+      "duration": 9.5,
+      1: ["12/05/2024", "04:00 AM", model1, 600, 371]
+    }
+  },
+  2: {
+    "name": "CHN - Guangzhou Baiyun International Airport",
+    "location": "Guangdong Province, China",
+    # Routes Available
+    1: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {}
+  },
+  3: {
+    "name": "GBR - London Heathrow Airport",
+    "location": "London, United Kingdom",
+    # Routes Available
+    1: {},
+    2: {},
+    4: {},
+    5: {},
+    6: {}
+  },
+  4: {
+    "name": "USA - Los Angeles International Airport",
+    "location": "California, USA",
+    # Routes Available
+    1: {},
+    2: {},
+    3: {},
+    5: {},
+    6: {}
+  },
+  5: {
+    "name": "PHL - Ninoy Aquino International Airport",
+    "location": "Manila, Philippines",
+    # Routes Available
+    1: {},
+    2: {},
+    3: {},
+    4: {},
+    6: {}
+  },
+  6: {
+    "name": "JPN - Tokyo Haneda Airport",
+    "location": "Tokyo, Japan",
+    # Routes Available
+    1: {},
+    2: {},
+    3: {},
+    4: {},
+    5: {}
+  }
+}
